13746f101f3cc13551b4017fc3475159955776d8
benjamin <benjamin@devforge>
2015-06-29 15:28:59 +1000

    Update

---
+++ src/Forge/Autoloader.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Application
+ *
+ * @category   Forge
+ * @package    Forge
+ * @subpackage Autoloader
+ * @author     Benjamin Tehan <purphoros@devforge.org>
+ * @copyright  Copyright (c) 2000-2015 Devforge.net (http://www.devforge.net)
+ * @license    http://www.devforge.org/php/license/
+ * @version    $Id$
+ * @link       https://github.com/phpforge/application
+ */
+namespace Forge;
+
+/**
+ * Application Class
+ *
+ * @category   Forge
+ * @package    Forge
+ * @subpackage Autoloader
+ * @author     Benjamin Tehan <purphoros@devforge.org>
+ * @copyright  Copyright (c) 2000-2015 Devforge.net (http://www.devforge.net)
+ * @license    http://www.devforge.org/php/license/
+ * @version    $Id$
+ * @link       https://github.com/phpforge/application
+ */
+class Autoloader {
+	
+}
